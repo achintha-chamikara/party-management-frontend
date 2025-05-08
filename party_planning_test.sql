0eb36af040d0cbd1f5913a4d1de43e6754798fc4
Savindya Thathsarani <savindya7874@gmail.com>
2025-05-08 15:41:20 +0530

    updated database

--- party_planning_test.sql
+++ party_planning_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2025 at 06:59 AM
+-- Generation Time: May 08, 2025 at 12:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `ad_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
   `First_Name` varchar(100) DEFAULT NULL,
   `Last_Name` varchar(100) DEFAULT NULL,
   `UserName` varchar(50) NOT NULL,
@@ -39,8 +39,9 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`ad_id`, `First_Name`, `Last_Name`, `UserName`, `Password`) VALUES
-(1, 'James', 'Carter', 'JamesCarter', 'James1234');
+INSERT INTO `admin` (`admin_id`, `First_Name`, `Last_Name`, `UserName`, `Password`) VALUES
+(1, 'James', 'Carter', 'admin123', '$2y$10$19G8S0K1lfLVRdjANUGxRu/p6X5Rp3PRy4YvsNE3wOgso0cCjc.kC'),
+(2, 'Sajeevan', 'Shan', 'Sajeevanshan', '$2y$10$M8F0n/Pnm/GO43Lezb36B.Izwts09QHZeKjHMnK22Ck1w3bSCMlWS');
 
 -- --------------------------------------------------------
 
@@ -57,7 +58,7 @@ CREATE TABLE `customer` (
   `Password` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `phone_number` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL
+  `admin_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -65,7 +66,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`cus_id`, `First_Name`, `Last_Name`, `Age`, `UserName`, `Password`, `Email`, `phone_number`, `admin_id`) VALUES
-(1, 'student\r\n\r\n', NULL, 23, 'USJ', 'CSC209', 'student123@gmail.com', 765648392, 1);
+(1, 'student\r\n\r\n', NULL, 23, 'USJ', 'CSC209', 'student123@gmail.com', 765648392, 1),
+(4, 'Savi', 'Sathsarani', 23, 'savindya', '$2y$10$cDVe2E82lk5PjOMmpBtmtO3whB95JJmLr0MuQz4kXmeq4y7FJC7Q.', 'savindya7874@gmail.com', 2147483647, 1);
 
 -- --------------------------------------------------------
 
@@ -74,6 +76,7 @@ INSERT INTO `customer` (`cus_id`, `First_Name`, `Last_Name`, `Age`, `UserName`, 
 --
 
 CREATE TABLE `feedback` (
+  `feedback_id` int(11) NOT NULL,
   `cus_id` int(11) NOT NULL,
   `message` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -82,8 +85,8 @@ CREATE TABLE `feedback` (
 -- Dumping data for table `feedback`
 --
 
-INSERT INTO `feedback` (`cus_id`, `message`) VALUES
-(1, 'The party planning site made organizing my event a breeze! From selecting vendors to coordinating details, everything was seamless. Highly recommend for stress-free party planning.');
+INSERT INTO `feedback` (`feedback_id`, `cus_id`, `message`) VALUES
+(1, 1, 'The party planning site made organizing my event a breeze! From selecting vendors to coordinating details, everything was seamless. Highly recommend for stress-free party planning.');
 
 -- --------------------------------------------------------
 
@@ -97,13 +100,23 @@ CREATE TABLE `reservation` (
   `time_from` time NOT NULL,
   `time_to` time NOT NULL,
   `party_type` varchar(50) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `no_of_guests` int(11) NOT NULL,
   `cus_id` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL,
-  `vendor_id` int(11) NOT NULL,
-  `venue_id` int(11) NOT NULL
+  `vendor_music_id` int(11) NOT NULL,
+  `vendor_photo_id` int(11) NOT NULL,
+  `vendor_food_id` int(11) NOT NULL,
+  `venue_id` int(11) NOT NULL,
+  `theme_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`res_id`, `date`, `time_from`, `time_to`, `party_type`, `description`, `no_of_guests`, `cus_id`, `vendor_music_id`, `vendor_photo_id`, `vendor_food_id`, `venue_id`, `theme_id`, `admin_id`) VALUES
+(1, '2025-04-30', '13:30:00', '15:30:00', 'birthday', 'I want to plan unforgettable bday party with my friends, I\'m a teenager', 5, 1, 3, 2, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -147,7 +160,8 @@ CREATE TABLE `vendor` (
 
 INSERT INTO `vendor` (`vendor_id`, `name`, `description`, `fee`, `Email`, `category`, `admin_id`) VALUES
 (1, 'Amaya Foods', 'Mouth Watering Food, choose meal preferences, we are serviced lunch', 2000, 'amayafoods@gmail.com', 'Food', 1),
-(2, 'Vision', 'We covered all the special events of your life.contact us for more details. Generally Our fee is as mentioned for one hour', 3000, 'visionphotographers@gmail.com', 'Photo', 1);
+(2, 'Vision', 'We covered all the special events of your life.contact us for more details. Generally Our fee is as mentioned for one hour', 3000, 'visionphotographers@gmail.com', 'Photography', 1),
+(3, 'Thala', 'Elevate your event with the timeless elegance of our classical music ensemble. Specializing in weddings, corporate functions, and private parties, we bring sophistication and atmosphere through expertly performed string, piano, and orchestral arrangements.', 3000, 'thalamusic@gmail.com', 'Music', 1);
 
 -- --------------------------------------------------------
 
@@ -179,7 +193,7 @@ INSERT INTO `venue` (`venue_id`, `name`, `description`, `website`, `admin_id`) V
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`ad_id`);
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `customer`
@@ -192,6 +206,7 @@ ALTER TABLE `customer`
 -- Indexes for table `feedback`
 --
 ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`),
   ADD KEY `cus_id` (`cus_id`);
 
 --
@@ -200,9 +215,12 @@ ALTER TABLE `feedback`
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`res_id`),
   ADD KEY `cus_id` (`cus_id`),
-  ADD KEY `admin_id` (`admin_id`),
-  ADD KEY `vendor_id` (`vendor_id`),
-  ADD KEY `venue_id` (`venue_id`);
+  ADD KEY `admin_id` (`vendor_music_id`),
+  ADD KEY `vendor_id` (`vendor_photo_id`),
+  ADD KEY `theme_id` (`theme_id`),
+  ADD KEY `vendor_food_id` (`vendor_food_id`),
+  ADD KEY `venue_id` (`venue_id`),
+  ADD KEY `admin_id_2` (`admin_id`);
 
 --
 -- Indexes for table `theme`
@@ -233,19 +251,25 @@ ALTER TABLE `venue`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `res_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `res_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `theme`
@@ -257,7 +281,7 @@ ALTER TABLE `theme`
 -- AUTO_INCREMENT for table `vendor`
 --
 ALTER TABLE `vendor`
-  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `venue`
@@ -273,7 +297,7 @@ ALTER TABLE `venue`
 -- Constraints for table `customer`
 --
 ALTER TABLE `customer`
-  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `feedback`
@@ -286,27 +310,30 @@ ALTER TABLE `feedback`
 --
 ALTER TABLE `reservation`
   ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`vendor_id`) REFERENCES `vendor` (`vendor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_4` FOREIGN KEY (`venue_id`) REFERENCES `venue` (`venue_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`vendor_music_id`) REFERENCES `vendor` (`vendor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`vendor_photo_id`) REFERENCES `vendor` (`vendor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_4` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`theme_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_5` FOREIGN KEY (`vendor_food_id`) REFERENCES `vendor` (`vendor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_6` FOREIGN KEY (`venue_id`) REFERENCES `venue` (`venue_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_7` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `theme`
 --
 ALTER TABLE `theme`
-  ADD CONSTRAINT `theme_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `theme_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `vendor`
 --
 ALTER TABLE `vendor`
-  ADD CONSTRAINT `vendor_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `vendor_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `venue`
 --
 ALTER TABLE `venue`
-  ADD CONSTRAINT `venue_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `venue_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
